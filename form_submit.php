57a146cc322b21b94267d39d2966698e453d0ff2
sgabbard <sarahngabbard@gmail.com>
2016-01-21 15:04:33 -0600

    updated validation

--- form_submit.php
+++ form_submit.php
@@ -5,7 +5,20 @@
         <meta charset="utf-8" />
 		<link rel="stylesheet" href="feature_request_style.css" type="text/css" media="screen" />
 		<?php
-			function buildProductList($cat_id) {
+			if ($_POST['_submit_check']) {
+			// If validate_form() returns errors, pass them to show_form()
+				if ($form_errors = validate_form()) {
+					show_form($form_errors);
+				} else {
+					// The data sent is valid, hence process it...
+					insertFeatureRequest();
+				} else {
+					// The form has not been sent, hence show it again...
+					show_form();
+				}
+			}
+			
+			function insertFeatureRequest($cat_id) {
 			$host="localhost";
 			$port=3306;
 			$socket="";
@@ -25,4 +38,7 @@
 				exit;
 			}
 	</head>
+	<body>
+	</body>
+</html>
 	
